f72b65a0b4fe5199b9ef6b179b5336113ad979d2
SheikhAahil <aahilakhtar001@gmail.com>
2025-09-14 16:07:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Animation for the "HAPPY BIRTHDAY MOTKI" title
+window.onload = () => {
+  const title = document.querySelector('.birthday-title');
+  title.style.animation = "fadeIn 3s ease-in-out";
+};
